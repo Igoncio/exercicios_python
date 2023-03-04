97c72b8536b19be823b150ae2c0fdd53f6a593cb
Igor <120050260+Igoncio@users.noreply.github.com>
2023-03-04 02:47:05 -0400

    Update exercicio_13.py

--- exercicio_13.py
+++ exercicio_13.py
@@ -1,1 +1,10 @@
+#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja invalido e continue pedindo até que o usuario informe um valor valido.
 
+while True:
+    valor = float(input("Digite um valor entre zero e dez: "))
+    if valor <= 0 and valor <= 10:
+        print("Valor válido")
+
+    if valor > 0 and valor > 10:
+        print("valor invalido")
+        break
