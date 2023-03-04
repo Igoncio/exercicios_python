6fe2484bcba8e85dcc64d1d562aa96b2bc7c86c4
Igor <120050260+Igoncio@users.noreply.github.com>
2023-03-04 02:46:36 -0400

    Update exercicio_12.py

--- exercicio_12.py
+++ exercicio_12.py
@@ -1,1 +1,6 @@
+#Algoritimo que conta quantas vezes se pode reduzir em 1 o valor do numero passado como parâmetro até chegar a zero
 
+num = 9999999999999999999
+while (num >= 0):
+    print(num)
+    num = num - 1
